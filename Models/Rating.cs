3a8b9db3266ba5f03ebd9ff95c23155f1bf335ad
Rob Rupp <robruppjr@gmail.com>
2022-05-18 21:51:55 -0400

    Accessimg Database Complete

---
+++ Models/Rating.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("Restaurant")]
+        public int RestaurantId { get; set; }
+        [Required]
+        public double Score { get; set; }
+    }
---
+++ Models/Restaurant.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+    public class Restaurant
+    {
+        [Key]
+        public int ID {get; set;}
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength (100)]
+        public string Location { get; set; }
+    }
